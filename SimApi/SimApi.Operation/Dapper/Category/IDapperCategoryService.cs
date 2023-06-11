06aeb897d6b22f27134c9be9c4c27cf5ecf25fa9
elifercann <ercanelif450@gmail.com>
2023-06-11 18:12:22 +0300

    dosyalar eklendi

---
+++ SimApi/SimApi.Operation/Dapper/Category/IDapperCategoryService.cs
@@ -0,0 +1,15 @@
+﻿using SimApi.Base;
+using SimApi.Data;
+using SimApi.Schema;
+
+namespace SimApi.Operation.Dapper;
+
+public interface IDapperCategoryService : IBaseService<Category, CategoryRequest, CategoryResponse>
+{
+    //ApiResponse<List<CategoryResponse>> GetAll();
+    //ApiResponse<CategoryResponse> GetById(int id);
+    //ApiResponse<CategoryResponse> Insert(CategoryRequest request);
+    //ApiResponse<CategoryResponse> Update(int id,CategoryRequest request);
+    //ApiResponse<CategoryResponse> Delete(int id);
+
+}
---
+++ SimApi/SimApi.Service/Autofac/AutofacBussinesModule.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using SimApi.Operation;
+using SimApi.Operation.Dapper;
+using SimApi.Service.CustomService;
+
+namespace SimApi.Service.Autofac
+{
+    public class AutofacBussinesModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<UserLogService>().As<IUserLogService>().InstancePerLifetimeScope();
+            builder.RegisterType<TokenService>().As<ITokenService>().InstancePerLifetimeScope();
+            builder.RegisterType<UserService>().As<IUserService>().InstancePerLifetimeScope();
+            builder.RegisterType<CustomerService>().As<ICustomerService>().InstancePerLifetimeScope();
+            builder.RegisterType<AccountService>().As<IAccountService>().InstancePerLifetimeScope();
+            builder.RegisterType<TransactionService>().As<ITransactionService>().InstancePerLifetimeScope();
+
+            builder.RegisterType<TransactionReportService>().As<ITransactionReportService>().InstancePerLifetimeScope();
+            builder.RegisterType<DapperAccountService>().As<IDapperAccountService>().InstancePerLifetimeScope();
+            builder.RegisterType<DapperCategoryService>().As<IDapperCategoryService>().InstancePerLifetimeScope();
+
+            builder.RegisterType<ScopedService>().InstancePerLifetimeScope();
+            builder.RegisterType<TransientService>().InstancePerDependency();
+            builder.RegisterType<SingletonService>().SingleInstance();
+        }
+        
+    }
+}
